e328ee66a513bede40bf78024b4fdd92793bcbae
oldUath <haiyangdong666@gmail.com>
2020-11-07 23:42:14 +0800

    'props传参只能读'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state={
       x:0
     }
   };
-  add(){
+  add=()=>{
     this.setState({
         x:this.state.x+1
     })
@@ -16,10 +16,25 @@ class App extends React.Component {
     return(
       <div>
         APP
-         x:{this.state.x}
          <button onClick={()=>this.add()}>+1</button>
+          <B name={this.state.x} />
       </div>
     )};
+}
+class B extends React.Component{
+    //每次props改变时，就会启用这个钩子
+    componentWillReceiveProps(nextProps, nextContext) {
+        console.log('老props')
+        console.log(this.props)
+        console.log('new')
+        console.log(nextProps)
+    }
+
+    render() {
+        return(
+            <div>{this.props.name}</div>
+        )
+    }
 }
 
 export default App;
